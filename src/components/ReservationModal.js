6bbe82caf92eb6b3e4d5894c450b35d6b7da6235
Gabriel <bielferreirafernandes06@gmail.com>
2025-01-28 15:28:54 -0300

    Removal of RG in booking details

--- src/components/ReservationModal.js
+++ src/components/ReservationModal.js
@@ -1094,7 +1094,6 @@ const ReservationModal = ({
               >
                 <option value="">Selecione</option>
                 <option value="cpf">CPF</option>
-                <option value="rg">RG</option>
                 <option value="passport">Passaporte</option>
               </StyledSelect>
             </FieldValue>
@@ -1278,7 +1277,6 @@ const ReservationModal = ({
                     >
                       <option value="">Tipo Documento</option>
                       <option value="cpf">CPF</option>
-                      <option value="rg">RG</option>
                       <option value="passport">Passaporte</option>
                     </StyledSelect>
                   </FieldValue>
